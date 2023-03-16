48fc33f95c5d72b46a0d7ad073bb2b145617cc03
Akshay <akalekar2003@gmail.com>
2023-03-16 11:47:39 +0530

    Structured

--- Components/Hero/Hero.jsx
+++ Components/Hero/Hero.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <div>
+        {/*Left Side*/}
+        <div>
+          <div>Let’s Connect with people in tech</div>
+          <div>A group for peer learning which keeps the community engaged</div>
+          <button> Join Discord </button>
+        </div>
+        {/*Right Side */}
+        <div>
+          <img src=""/>
+        </div>
+      </div>
 
-export default Hero
+  { /*Two Big Card*/}
+      <div>
+      <Card Title={"Why DevMatrix"} Description={"A group of individuals with similar interests and goals join forces to support and develop each other through shared knowledge, experiences, and resources. "} />
+      <Card Title={"Our Concept"} Description={"Our emphasis is on quality rather than quantity. Your commitment to consistent growth is our primary requirement. Persistence and hard work are crucial to achieving success."}/>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
